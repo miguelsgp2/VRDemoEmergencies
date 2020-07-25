ba5f40e727e792b067cca7b3a9211d07d858a9ca
miguelsgp2 <68101479+miguelsgp2@users.noreply.github.com>
2020-07-25 21:48:04 +0100

    Barrel Imported from Assets

--- VRDemoEmergencies/Assets/Scripts/CollectingBarrelsVolume.cs
+++ VRDemoEmergencies/Assets/Scripts/CollectingBarrelsVolume.cs
@@ -3,10 +3,12 @@
 public class CollectingBarrelsVolume : MonoBehaviour
 {
     private AudioSource alarmAudioSource;
+    private AudioSource audioSource;
 
     private void Start()
     {
-        alarmAudioSource = AudioManager.instance.
+        //alarmAudioSource = AudioManager.instance.
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
